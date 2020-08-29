00cdd66c5ffaf3eb804e7d2aa29306354268ff3a
toranosuke0527 <18cge0003kt@edu.sendai-com.ac.jp>
2020-08-29 18:02:46 +0900

    プロジェクトファイルup

---
+++ Start/Assets/Script/UIManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+
+    [SerializeField] GameObject menuPanel;
+    [SerializeField] GameObject StagePanel;
+    [SerializeField] GameObject BicyclePanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void SelectXrHubDescription() 
+    {
+        menuPanel.SetActive(false);
+        StagePanel.SetActive(true);
+    }
+
+    public void SelectUnityDescription() 
+    {
+        StagePanel.SetActive(false);
+        BicyclePanel.SetActive(true);
+    }
+}
---
+++ Start/Assets/Script/test.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0,-0.5f,0));
+    }
+}
